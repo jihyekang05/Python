01c4de5f94ab2b8b66173cdf7ee0b77df30b8138
JIHYE KANG <62206966+jihyekang05@users.noreply.github.com>
2022-07-23 16:03:17 +0900

    Create 증가수열만들기.py

---
+++ 증가수열만들기.py
@@ -0,0 +1,61 @@
+
+
+import sys
+sys.stdin=open("input.txt", "r")
+
+N = int(input())
+a = list(map(int,input().split()))
+last = 0
+lt = 0
+rt = N-1
+res = ""
+tmp =[]
+
+while lt<=rt:
+    if a[lt] > last:
+        tmp.append((a[lt],'L'))
+    if a[rt] > last:
+        tmp.append((a[rt],'R'))
+    tmp.sort()
+    if len(tmp) == 0:
+        break
+    else:
+        res = res+tmp[0][1]
+        last=tmp[0][0]
+        if tmp[0][1] == 'L':
+            lt +=1
+        else:
+            rt-=1
+    tmp.clear()
+
+print(len(res))
+print(res)
+
+
+
+
+
+    
+    
+
+
+
+
+            
+
+        
+            
+           
+
+
+  
+    
+
+       
+    
+
+
+
+
+
+ 
