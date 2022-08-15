4cf2f8afc980e518114ff8f11bec2210b8de7104
JIHYE KANG <62206966+jihyekang05@users.noreply.github.com>
2022-08-15 14:49:32 +0900

    Update 기능개발.py

--- 프로그래머스/기능개발.py
+++ 프로그래머스/기능개발.py
@@ -30,5 +30,49 @@ for i in range(3):
 print(answer)
 
 
+##최종코드
+
+progresses = [95, 90, 99, 99, 80, 99]
+speeds =[1, 1, 1, 1, 1, 1]
+n = 100
+a=0
+b=0
+res=[]
+answer=[]
+cnt=0
+
+
+
+for i in range(len(progresses)):
+    a = n-progresses[i]
+    b=a//speeds[i]
+    c=a%speeds[i]
+    if(c>0):
+        b+=1
+    res.append(b)
+
+norm=0
+for i in range(len(res)):
+    if(norm<res[i]):
+        norm=res[i]
+        cnt=0
+        cnt+=1
+        answer.append(cnt)
+        
+    else:
+        norm-=res[i]
+        cnt+=1
+        print(cnt)
+        answer[len(answer)-1]=cnt
+        
+            
+print(res)
+print(answer)
+
+
+
+
+
+
 
 
